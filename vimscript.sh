fdf585a9f48bfd55f2f02082841e0a87d748cc35
artnovak <artnovak1993@gmail.com>
2022-01-27 13:04:40 +1000

    Fixed wrong symbols

--- vimscript.sh
+++ vimscript.sh
@@ -2,7 +2,7 @@
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 echo  "call plug#begin()" >> ~/.vimrc
-echo  "Plug /'scrooloose/nerdtree'/, { 'on':  'NERDTreeToggle' }" >> ~/.vimrc
+echo  "Plug 'scrooloose/nerdtree', { 'on':  'NERDTreeToggle' }" >> ~/.vimrc
 echo  "\"colorschemes >>" ~/.vimrc
 echo  "Plug 'morhetz/gruvbox'" >> ~/.vimrc
 echo  "call plug#end()" >> ~/.vimrc
